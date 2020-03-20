bc4f7105324bc89c1d7eaec5b680e23a7babad6b
SHASHANK K HOLLA <shashankkholla.ec17@rvce.edu.in>
2020-03-20 11:51:35 +0530

    Routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'blog', component: BlogsComponent },
+
 ];
 
 @NgModule({
